7508a6ff698c774374398239aa5d11b1308edc56
Morethanafeeling <Morethanafeeling@126.com>
2022-10-28 16:01:37 +0800

    测试冲突

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("dev分支添加");
+        System.out.println("添加新的内容测试冲突");
     }
 }
