bdab285124adbb420eedcd45e17a1f27beaeca16
Morethanafeeling <Morethanafeeling@126.com>
2022-10-28 15:56:06 +0800

    2 oprations

--- src/main/java/com/atguigu/git/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.git;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("You Rock!");
-    }
-}
